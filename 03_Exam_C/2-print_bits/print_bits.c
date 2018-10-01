69c9fddf253ba7359297b7c22452274d2f844f22
UniKlo <uniklo@UniKlodeMacBook-Pro.local>
2018-10-01 00:16:46 -0700

    10/01/2018

--- 03_Exam_C/2-print_bits/print_bits.c
+++ 03_Exam_C/2-print_bits/print_bits.c
@@ -4,15 +4,34 @@
 void	print_bits(unsigned char octet)
 {
   int	cmp;
+  int	len;
   
-  cmp = 128;
-  while(cmp > 0)
+  cmp = 1;
+  len = 7;
+  while(len > -1)
     {
-      if (octet & cmp)
+      if (octet & cmp << len)
 	write(1, "1", 1);
       else
 	write(1, "0", 1);
-      cmp /= 2;
+      len -= 1;
+      printf("len: %d\n", len);
+    }
+}
+
+
+void    print_bits1(unsigned char octet)
+{
+  int   cmp;
+
+  cmp = 128;
+  while(cmp)
+    {
+      if (octet & cmp)
+        write(1, "1", 1);
+      else
+        write(1, "0", 1);
+      cmp /= 2;;
       printf("cmp: %d\n", cmp);
     }
 }
