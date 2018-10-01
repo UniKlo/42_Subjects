13116b774be39f82d972bc3cb4c37090d3ef52a8
Khloe Hou <khou@e1z4r13p1.42.us.org>
2018-10-01 13:01:27 -0700

    10/01/2018

--- 03_Exam_C/2-print_bits/print_bits.c
+++ 03_Exam_C/2-print_bits/print_bits.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   print_bits.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: khou <marvin@42.fr>                        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/10/01 12:38:52 by khou              #+#    #+#             */
+/*   Updated: 2018/10/01 12:56:34 by khou             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <unistd.h>
 #include <stdio.h>
 
@@ -22,16 +34,30 @@ void	print_bits(unsigned char octet)
 
 void    print_bits1(unsigned char octet)
 {
-  int   cmp;
+  unsigned char	cmp;
 
-  cmp = 128;
+  cmp = 128;// cmp = 1 << 7;
   while(cmp)
     {
       if (octet & cmp)
         write(1, "1", 1);
       else
         write(1, "0", 1);
-      cmp /= 2;;
+      cmp /= 2;
       printf("cmp: %d\n", cmp);
     }
+}
+
+void    print_bits2(unsigned char octet)
+{
+	if (octet > 0)
+		print_bits2(octet / 2);
+	printf("%d", octet % 2);
+}
+
+void    print_bits3(unsigned char octet)
+{
+    if (octet > 1)
+        print_bits3(octet >> 1);
+    printf("%d", octet & 1);
 }
