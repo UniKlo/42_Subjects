b5e5c8cd4e2a58e66e5dc32fd03d71f314cadb51
Khloe Hou <khou@e1z4r13p1.42.us.org>
2018-10-01 14:11:05 -0700

    10/01/2018

--- 03_Exam_C/2-print_bits/print_bits.c
+++ 03_Exam_C/2-print_bits/print_bits.c
@@ -6,14 +6,13 @@
 /*   By: khou <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/01 12:38:52 by khou              #+#    #+#             */
-/*   Updated: 2018/10/01 12:56:34 by khou             ###   ########.fr       */
+/*   Updated: 2018/10/01 14:09:41 by khou             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
-#include <stdio.h>
 
-void	print_bits(unsigned char octet)
+void	print_bits1(unsigned char octet)
 {
   int	cmp;
   int	len;
@@ -27,12 +26,11 @@ void	print_bits(unsigned char octet)
       else
 	write(1, "0", 1);
       len -= 1;
-      printf("len: %d\n", len);
     }
 }
 
 
-void    print_bits1(unsigned char octet)
+void    print_bits2(unsigned char octet)
 {
   unsigned char	cmp;
 
@@ -44,20 +42,5 @@ void    print_bits1(unsigned char octet)
       else
         write(1, "0", 1);
       cmp /= 2;
-      printf("cmp: %d\n", cmp);
     }
-}
-
-void    print_bits2(unsigned char octet)
-{
-	if (octet > 0)
-		print_bits2(octet / 2);
-	printf("%d", octet % 2);
-}
-
-void    print_bits3(unsigned char octet)
-{
-    if (octet > 1)
-        print_bits3(octet >> 1);
-    printf("%d", octet & 1);
 }
