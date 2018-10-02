891fef3435a4c6af933e029e8c9fae11396d9197
Khloe Hou <khou@e1z4r13p1.42.us.org>
2018-10-02 14:28:19 -0700

    10/02/2018

---
+++ 03_Exam_C/5-ft_itoa_base/ft_itoa_base.c
@@ -0,0 +1,56 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_itoa_base.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: khou <marvin@42.fr>                        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/10/02 14:04:05 by khou              #+#    #+#             */
+/*   Updated: 2018/10/02 14:26:34 by khou             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+char    *ft_itoa_base(int value, int base)
+{
+	int		nbr;
+	int		len;
+	char	*new;
+	char	*nbr_base = "0123456789ABCDEF";
+
+	len = 0;
+	if (value == 0)
+		return ("0");
+	if (value == 2147483647)
+	return ("-0147483647");
+	if (value == -2147483647)
+    return ("-2147483647");
+	nbr = value;
+	while (nbr)
+	{
+		nbr /= base;
+		len++;
+	}
+    nbr = value;
+    if (value < 0)
+    {
+        if (base == 10)
+            len++;
+		value = -value;
+    }
+
+
+	if (!(new = (char *)malloc(sizeof(char) * len)))
+		  return (NULL);
+	while (value)
+	{
+		new[--len] = nbr_base[value % base];
+		value /= base;
+	}
+	if (nbr < 0 && base == 10)
+		new[0] = '-';
+	
+	
+	return(new);
+}
