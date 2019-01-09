fac66f5149af257c78311c54e2a7ed1b4ff0f402
Khloe Hou <khou@e1z3r1p5.42.us.org>
2019-01-09 15:44:11 -0800

    01/09/19

--- 03_Exam_C/3-paramsum/paramsum.c
+++ 03_Exam_C/3-paramsum/paramsum.c
@@ -1,45 +1,31 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   inter.c                                            :+:      :+:    :+:   */
+/*   paramsum.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: khou <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/01/09 13:09:20 by khou              #+#    #+#             */
-/*   Updated: 2019/01/09 15:07:56 by khou             ###   ########.fr       */
+/*   Created: 2019/01/09 15:07:22 by khou              #+#    #+#             */
+/*   Updated: 2019/01/09 15:20:46 by khou             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-int main(int argc, char **argv)
+void	putnbr(int nbr)
 {
-	char str[126];
-	if(argc == 3)
-	{
-		int n = 0;
-		
-		while (n <= 126)
-			str[n++] = 0;
-			
-		while (*argv[1])
-		{
-			int beg = 0;
-			while (argv[2][beg])
-			{
-				if (*argv[1] == argv[2][beg])
-				{
-					if (!str[(int)*argv[1]])
-					{
-						str[(int)*argv[1]] = 1;
-						write (1, argv[1], 1);
-					}
-				}
-				beg++;
-			}
-			argv[1]++;
-		}
-	}
+	char c;
+	
+	if (nbr/10)
+		putnbr(nbr/10);
+	c = nbr % 10 + '0';
+	write(1, &c, 1);
+}
+
+int		 main(int argc, char **argv)
+{
+	argv[0] = "bicha";
+	putnbr(argc - 1);
 	write (1, "\n", 1);
 	return (0);
 }
