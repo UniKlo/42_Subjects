45b87b2d086a32491aefeb28e8def63a58a1412b
Khloe Hou <khou@e1z3r2p5.42.us.org>
2019-01-17 19:37:48 -0800

    01/17/19

--- 03_Exam_C/5-ft_itoa_base/ft_itoa_base.c
+++ 03_Exam_C/5-ft_itoa_base/ft_itoa_base.c
@@ -6,7 +6,7 @@
 /*   By: khou <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/17 16:56:58 by khou              #+#    #+#             */
-/*   Updated: 2019/01/17 18:06:14 by khou             ###   ########.fr       */
+/*   Updated: 2019/01/17 19:34:50 by khou             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,8 +38,11 @@ char    *ft_itoa_base(int nbr, int base)
 	int end = 0;
 	if (nbr < 0)
 	{
-		str[0] = '-';
-		end++;
+		if (base == 10)
+		{
+			str[0] = '-';
+			end++;
+		}
 		nbr = -nbr;
 	}
 	end += nbrlen(nbr, base);
@@ -52,13 +55,3 @@ char    *ft_itoa_base(int nbr, int base)
 	}
 	return(str);
 }
-
-/*
-#include <stdio.h>
-
-int		main()
-{
-	printf("nbr: %s\n", ft_itoa_base(-2147483648, 10)) ;
-	
-}
-*/
