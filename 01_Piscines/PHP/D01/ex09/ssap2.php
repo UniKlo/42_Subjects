5bee28783b66431f1696a9f237d155f205875dc3
Khloe Hou <khou@e1z3r2p5.42.us.org>
2019-03-27 20:34:09 -0700

    3/27/2019

--- 01_Piscines/PHP/D01/ex09/ssap2.php
+++ 01_Piscines/PHP/D01/ex09/ssap2.php
@@ -1,41 +1,50 @@
 #!/usr/bin/php
-<?php
-function check_alpha($a){
-	if (($a >= 'A' && $a <='Z') ||
-		($a >= 'a' && $a <= 'z'))
-		return (1);
-	return (0);
-}
-function ft_sort($a, $b){
-	$i = 0;
-	if (!$a) return (1);
-	if (!$b) return (-1);
-	while ($a[$i] && $b[$i] && $a[$i] == $b[$i])
-	{
-		$i++;
-		if ($i == strlen($a)) return (-1);
-		if ($i == strlen($b)) return (1);
-	}
-	if (check_alpha($a[$i]) && !check_alpha($b[$i])) return (-1);
-	elseif (check_alpha($b[$i]) && !check_alpha($a[$i])) return (1);
-	elseif (is_numeric($a[$i]) && !is_numeric($b[$i])) return (-1);
-	elseif (is_numeric($b[$i]) && !is_numeric($a[$i])) return (1);
-	if (check_alpha($a[$i]) && check_alpha($b[$i]))
-	{
-		$tmp_a = strtolower($a[$i]);
-		$tmp_b = strtolower($b[$i]);
-		return (($tmp_a > $tmp_b) ? 1 : -1);
-	}
-	return (($a[$i] > $b[$i]) ? 1 : -1);
-}
-$arr = array();
-for ($i = 1; $i < $argc; $i++)
+<?PHP
+function sort_arr($arrin)
 {
-	$tmp = preg_split('/\s+/', $argv[$i]);
-	for ($j = 0; $j < count($tmp); $j++)
-		array_push($arr, $tmp[$j]);
+	unset($arrin[0]);
+	$arr = array();
+	foreach ($arrin as $v)
+	{
+		$split = array_filter(explode(" ", $v));
+		foreach ($split as $v2)
+			$arr[] = $v2;
+	}
+	sort($arr);
+	return ($arr);
 }
-usort($arr, 'ft_sort');
-for ($i = 0; $i < count($arr); $i++)
-	echo ($arr[$i] . "\n");
+
+function new_sort($x, $y)
+{
+	$i = 0;
+	$a = strtolower($x);
+	$b = strtolower($y);
+	$comp = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz\"
+		012345678!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+	while (($i < strlen($a) || ($i < strlen($b))))
+	{
+		$posa = strpos($comp, $a[$i]);
+		$posb = strpos($comp, $b[$i]);
+		if ($posa < $posb)
+			return (-1);
+		else if ($posa > $posb)
+			return (1);
+		else
+			$i++;
+	}
+}
+
+if ($argc > 1)
+{
+	$oldsort = sort_arr($argv);
+	usort($oldsort, "new_sort");
+	foreach ($oldsort as $v2)
+		echo ("$v2\n");
+/*
+// case INsensitive
+// alphabetical order first
+// then numbers
+// then finally all the other characters
+ */
+}
 ?>
