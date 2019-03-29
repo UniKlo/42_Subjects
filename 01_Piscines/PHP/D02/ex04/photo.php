2fec37c4bd2ca147529dbc8a56c48412dc5a9ae7
Khloe Hou <khou@e1z3r2p5.42.us.org>
2019-03-28 22:10:32 -0700

    3/28/2019

---
+++ 01_Piscines/PHP/D02/ex04/photo.php
@@ -0,0 +1,27 @@
+#!/usr/bin/php
+<?php
+if ($argc != 2)
+	exit();
+$url = $argv[1];
+// $url = ("http://" . $input);
+$input = ltrim($url, "http://");
+$text = file_get_contents($url); 
+preg_match_all('/img.*src="(.*)"/U', $text, $match);
+//print_r($match);
+$dir_path = (getcwd() . '/' . $input);
+if (!file_exists($dir_path)) mkdir($dir_path, 0755);
+chdir($dir_path);
+foreach ($match[1] as $img)
+{
+	if (!strstr($img, "http:")) $img = ($url . $img);
+	echo ($img . "\n");
+	$curl = curl_init($img);
+	$file_name = basename($img);
+	$file = fopen($file_name, 'wb');
+	curl_setopt($curl, CURLOPT_FILE, $file);
+	curl_setopt($curl, CURLOPT_URL, $img);
+	curl_exec($curl);
+	curl_close($curl);
+	fclose($file);
+}
+?>
