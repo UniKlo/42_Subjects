f55f35cb63bba41ceea64061d7d8be80c93aa390
Khloe Hou <khou@e1z1r5p23.42.us.org>
2019-08-04 18:23:47 -0700

    08/04/2019

--- 03_Exam_C/Intermediate/0-find_pivot/find_pivot.c
+++ 03_Exam_C/Intermediate/0-find_pivot/find_pivot.c
@@ -6,7 +6,7 @@
 /*   By: khou <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/04 00:41:28 by khou              #+#    #+#             */
-/*   Updated: 2019/08/04 01:08:23 by khou             ###   ########.fr       */
+/*   Updated: 2019/08/04 18:22:07 by khou             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,27 +14,25 @@ int find_pivot(int *arr, int n)
 {
 	int r_sum = 0;
 	int l_sum = 0;
-	int i = 0;
+    int i = 1;
 
-	while (i < n)
-	{
-		r_sum += arr[i];
-		i++;
-	}
-	i = 0;
-	while (i < n)
-	{
-		if ((i - 1) > -1)
-		{
-			l_sum += arr[i - 1];
-			r_sum -= arr[i - 1] + arr[i];
-		}
+    while (i < n)
+    {
+        r_sum += arr[i];
+        i++;
+    }
+    i = 1;
+    while (i < n)
+    {
+		l_sum += arr[i - 1];
+		r_sum -= arr[i];
 		if (l_sum == r_sum)
 			return (i);
-		i++;
-	}
-	return (-1);
+        i++;
+    }
+    return (-1);
 }
+
 /*
 #include <stdio.h>
 int     main()
