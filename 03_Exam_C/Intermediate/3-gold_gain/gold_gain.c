5560271d60a8ddd1ada7d39409a7313c659f510b
Khloe Hou <khou@e1z1r8p7.42.us.org>
2019-08-18 18:09:35 -0700

    08/18/2019

--- 03_Exam_C/Intermediate/3-gold_gain/gold_gain.c
+++ 03_Exam_C/Intermediate/3-gold_gain/gold_gain.c
@@ -5,103 +5,135 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: exam <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/07/23 11:05:22 by exam              #+#    #+#             */
-/*   Updated: 2019/07/23 12:40:04 by exam             ###   ########.fr       */
+/*   Created: 2019/08/13 10:55:26 by exam              #+#    #+#             */
+/*   Updated: 2019/08/18 18:08:35 by khou             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 
-int max_ar(int a, int b, int c)
+int	biggest(int a, int b, int c)
 {
-	int max = -1;
-	if (a > max)
+	int max = 0;
+
+	if (a > b)
 		max = a;
-    if (b > max)
-        max = b;
-    if (c > max)
-        max = c;
-	return (max);
+	else 
+		max = b;
+
+	if (max > c)
+		return (max);
+	return (c);
 }
 
-int gold_gain(int **mine, int n)
+int	gold_gain(int **mine, int n)
 {
-	int **tab = malloc(sizeof (int *) * n);
+	int **gain = malloc(sizeof(int *) * n);
 	int i = 0;
-
-    while (i < n)
-    {
-		tab[i]= malloc(sizeof (int) * n);
-        int j = 0;
-        while (j < n)
-        {
-            tab[i][j] = 0;
-            j++;
-        }
-        i++;
-    }
-	int max = -1;
 	int j = 0;
-    while (j < n)
+	while (i < n)
+	{
+		gain[i] = malloc(sizeof(int) * n);
+		j = 0;
+		while (j < n)
+		{
+			gain[i][j] = mine[i][j];
+			j++;
+		}
+		i++;
+	}
+	i = 0;
+	j = 0;
+	int ret = 0;
+	while (j < n)
     {
         i = 0;
-		while (i < n)
-		{
-			int rt = 0;
-			int r = 0;
-			int rd = 0;
-            if (i - 1 > -1 && j - 1 > -1)
-                rt += mine[i - 1][j - 1];
-			if (j - 1 > -1)
-				r += mine[i][j - 1];
-            if (i + 1 < n && j - 1 > -1)
-                rd += mine[i + 1][j - 1];
-
-            tab[i][j] = max_ar(rt, r, rd) + mine[i][j];
-			mine[i][j] = tab[i][j];
-			if (tab[i][j] > max)
-				max = tab[i][j]; 
+        while (i < n)
+        {
+			int t = 0;
+			int m = 0;
+			int b = 0;
+			if (i - 1 >= 0 && j - 1 >= 0)
+				t = gain[i - 1][j - 1];
+			if (j - 1 >= 0)
+				m = gain[i][j-1];
+			if (i + 1 < n && j - 1 >= 0)
+				b = gain[i + 1][j - 1];
+            gain[i][j] = mine[i][j] + biggest(t, m ,b);
+			if (gain[i][j] > ret)
+				ret = gain[i][j];
             i++;
-		}
+        }
         j++;
     }
-	return (max);
-
+	return (ret);
 }
 
 /*
-int main()
+#include <stdio.h>
+
+void	print_map(int **tab, int n)
 {
-	int n = 3;
-	int **mine = malloc(sizeof (int *) * n);
-	mine[0] = malloc(sizeof (int) * n);
-	mine[1] = malloc(sizeof (int) * n);
-	mine[2] = malloc(sizeof (int) * n);
-
-	mine[0][0] = 1;
-	mine[0][1] = 2;
-	mine[0][2] = 3;
-
-    mine[1][0] = 3;
-    mine[1][1] = 4;
-    mine[1][2] = 8;
-
-    mine[2][0] = 9;
-    mine[2][1] = 6;
-    mine[2][2] = 7;
 	int i = 0;
 	while (i < n)
 	{
 		int j = 0;
 		while (j < n)
 		{
-			printf("%d ", mine[i][j]);
+			printf("%d ", tab[i][j]);
 			j++;
 		}
 		printf("\n");
 		i++;
-	} 
-	printf("********************\n");
-	printf("gold_gain: %d\n", gold_gain(mine, n));
+	}
+}
+
+int main()
+{
+	int n = 3;
+	int **tab = malloc(sizeof(int *) * n);
+
+	int i = 0;
+	while (i < n)
+	{
+		tab[i] = malloc(sizeof(int) * n);
+		i++;
+	}
+	
+	tab[0][0] = 1;
+	tab[0][1] = 3;
+	tab[0][2] = 1;
+	tab[0][3] = 5;
+
+	tab[1][0] = 2;
+    tab[1][1] = 2;
+    tab[1][2] = 4;
+    tab[1][3] = 1;
+
+	tab[2][0] = 5;
+    tab[2][1] = 0;
+    tab[2][2] = 2;
+    tab[2][3] = 3;
+
+	tab[3][0] = 0;
+    tab[3][1] = 6;
+    tab[3][2] = 1;
+    tab[3][3] = 2;
+	
+
+	tab[0][0] = 1;
+    tab[0][1] = 2;
+    tab[0][2] = 3;
+
+    tab[1][0] = 3;
+    tab[1][1] = 4;
+    tab[1][2] = 8;
+
+    tab[2][0] = 9;
+    tab[2][1] = 6;
+    tab[2][2] = 7;
+	print_map(tab, n);
+
+	printf("gold_gain: %d\n", gold_gain(tab, n));
 }
 */
