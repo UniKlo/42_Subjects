441dda4bd9cb77418e33839da871dd9c005491a6
Khloe Hou <khou@e1z1r7p20.42.us.org>
2019-08-19 23:22:35 -0700

    08/19/2019

--- 03_Exam_C/Intermediate/4-intersection/intersection.c
+++ 03_Exam_C/Intermediate/4-intersection/intersection.c
@@ -1,3 +1,14 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   intersection.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: khou <marvin@42.fr>                        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/08/19 23:14:34 by khou              #+#    #+#             */
+/*   Updated: 2019/08/19 23:20:18 by khou             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 typedef struct s_node {
   void *content;
@@ -7,6 +18,8 @@ typedef struct s_node {
 void *intersection(struct s_node *lst1, struct s_node *lst2)
 {
   t_node *tmp = lst2;
+  if (!lst1 || !lst2)
+	  return (0);
   while(lst1)
     {
       lst2 = tmp;
@@ -21,7 +34,7 @@ void *intersection(struct s_node *lst1, struct s_node *lst2)
   return (0);
 }
 
-
+/*
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -54,10 +67,18 @@ int	main()
 
   lst2 = new_node("one");
   lst2->next = new_node("two");
-   lst2->next->next = lst1->next->next;
-   //  lst2->next->next = new_node("three");
+  //lst2->next->next = lst1->next->next;
+  lst2->next->next = new_node("three");
   print_node(lst1);
   print_node(lst2);
-  printf("T/F: %s\n", intersection(lst1, lst2));
+  t_node *inter = intersection(lst1, lst2);
+  printf("inter is %p\n", inter);
+
+  t_node *lst3 = NULL;
+  t_node *lst4 = NULL;
+  t_node *inter1 = intersection(lst3, lst4);
+  printf("inter is %p\n", inter1);
+
 }
 
+*/
