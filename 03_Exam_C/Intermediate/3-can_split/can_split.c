5015a006bd5284c86447941096763dc08e72f502
Khloe Hou <khou@e1z1r7p20.42.us.org>
2019-08-19 22:47:59 -0700

    08/19/2019

--- 03_Exam_C/Intermediate/3-can_split/can_split.c
+++ 03_Exam_C/Intermediate/3-can_split/can_split.c
@@ -1,11 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   can_split.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: khou <marvin@42.fr>                        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/08/19 22:36:18 by khou              #+#    #+#             */
+/*   Updated: 2019/08/19 22:45:50 by khou             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 struct s_node 
 {
   int value;
   struct s_node *left;
   struct s_node *right;
 };
-
-#include <stdio.h>
 
 int	size_of_tree(struct s_node *root)
 {
@@ -24,13 +34,10 @@ int	half_size(struct s_node *root, int half)
   if (!root)
     return (0);
   size = half_size(root->right, half) + half_size(root->left, half) + 1;
-  printf("size: %d\n", size);
   if (size == half)
     mark = 1;
   return (size); 
 }
-
-
 
 int	can_split(struct s_node *root)
 {
@@ -44,7 +51,7 @@ int	can_split(struct s_node *root)
   return (0);
 }
 
-
+/*
 
 #include <stdlib.h>
 #include <stdio.h>
@@ -63,33 +70,50 @@ void	print_tree(struct s_node *root)
 {
   if (!root)
     return ;
-  printf("%d\n", root->value);
   print_tree(root->left);
   print_tree(root->right);
+  printf("%d\n", root->value);
 }
 
 int	main()
 {
   struct s_node *root;
-  /*
+  
   root = new_node(28);
   root->left = new_node(51);
   root->left->left = new_node(26);
   root->left->right = new_node(48);
   root->left->left->left = new_node(76);
   root->left->left->right = new_node(13);
-  */
-  root = new_node(5);
-  root->left = new_node(1);
-  root->right = new_node(6);
-  root->right->left = new_node(7);
-  root->right->right = new_node(4);
-  root->right->left->left = new_node(3);
-  root->right->left->right = new_node(2);
-  root->right->right->right = new_node(8);
   print_tree(root);
 
   printf("size of the tree: %d\n", size_of_tree(root));
   printf("can split: %d\n", can_split(root));
+  mark = 0;
+  struct s_node *root1;
+  root1 = new_node(5);
+  root1->left = new_node(1);
+  root1->right = new_node(6);
+  root1->right->left = new_node(7);
+  root1->right->right = new_node(4);
+  root1->right->left->left = new_node(3);
+  root1->right->left->right = new_node(2);
+  root1->right->right->right = new_node(8);
+  print_tree(root1);
+
+  printf("size of the tree: %d\n", size_of_tree(root1));
+  printf("can split: %d\n", can_split(root1));
+  
+  mark = 0;
+  struct s_node *root2;
+  root2 = new_node(5);
+  root2->left = new_node(1);
+  print_tree(root2);
+
+  printf("size of the tree: %d\n", size_of_tree(root2));
+  printf("can split: %d\n", can_split(root2));
+
+  
   return (0);
 }
+*/
